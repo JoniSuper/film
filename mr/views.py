a6bd6e33959c666193a7b5fb8abc5cf3b61adac8
ivan <ivan@ivan.Dlink>
2019-02-22 19:17:38 +0300

    first commit

--- mr/views.py
+++ mr/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from .models import newsIndex
 
 def index(request):
-    news = newsIndex.objects.all()
-    return render(request, 'mr/index.html', {'news': news})
+    newss = newsIndex.objects.all()
+    return render(request, 'mr/index.html', context={'newss': newss})
